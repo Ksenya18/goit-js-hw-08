bbb81f0fc3c09fd32b28f012ec6e80a0fb1847bb
Ksenya18 <106611808+Ksenya18@users.noreply.github.com>
2023-01-10 03:04:49 +0200

    goit-js-hw-08 updated

---
+++ src/js/01-gallery.js
@@ -0,0 +1,27 @@
+// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+function createCardMarkup(array) {
+    return array
+        .map((item) =>
+         `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+    )
+    .join("");
+}
+
+const CardMarkup = createCardMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", CardMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+
+console.log(galleryItems);
